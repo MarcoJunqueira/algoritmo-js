0b0cdc6f5d32719a00ac490768a5b6dd8a193365
MarcoJunqueira <marcoalbuquerque2311@gmail.com>
2025-03-31 09:27:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var nome = prompt("Insira seu nome: ");
+var nascimento = prompt("Insira o ano em que voçe nasceu: ");
+var ano = prompt("Insira o ano em que estamos: ")
+var idade = ano - nascimento
+
+alert(" Ola " + nome + " voçe tem " + idade + " anos ")
